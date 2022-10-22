892bb74fc946825d3b917685b89c612b110745e5
Avi241 <arvind444pandit@gmail.com>
2022-10-22 18:59:00 +0530

    updated_build

--- build.sh
+++ build.sh
@@ -3,19 +3,6 @@ set -e
 
 ROOT=$PWD
 
-# submodules
-git submodule init
-git submodule update
-
-cd ros_ws/src/crazyflie_tools/
-git submodule init
-git submodule update
-cd $ROOT
-
-cd ros_ws/src/crazyswarm/externalDependencies/libmotioncapture/
-git submodule init
-git submodule update
-cd $ROOT
 
 # build simulator firmware backend
 cd ros_ws/src/crazyswarm/scripts/pycrazyswarm/cfsim
